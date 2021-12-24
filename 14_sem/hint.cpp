293a098dea5d4da3f4c3dcb3db22e2dd71793182
Ivan <vantank36@gmial.com>
2021-12-24 19:07:04 +0300

    Added second example.

--- 14_sem/hint.cpp
+++ 14_sem/hint.cpp
@@ -25,7 +25,35 @@ void FirstExample(std::map<size_t, std::string> &some_map) {
     timeFinish = std::chrono::high_resolution_clock::now();
     elapsed = timeFinish - timeStart;
     std::cout << "First example elapsed hint time: " << elapsed.count() << " s\n";
+    some_map.erase(10000000000);
+}
 
+void SecondExample(std::map<size_t, std::string> &some_map) {
+    std::chrono::duration<double> elapsed;
+    auto timeStart = std::chrono::high_resolution_clock::now();
+
+    for (size_t i = 1; i < 1000; ++i) {
+        some_map.insert({10000000000 + i, "1000000000" + std::to_string(i)});
+    }
+
+    auto timeFinish = std::chrono::high_resolution_clock::now();
+    elapsed = timeFinish - timeStart;
+    std::cout << "Second example elapsed time: " << elapsed.count() << " s\n";
+
+    for (size_t i = 1; i < 6; ++i) {
+        some_map.erase(10000000000 + i);
+    }
+
+    timeStart = std::chrono::high_resolution_clock::now();
+
+    auto help = some_map.insert({10000000001, "10000000001"});
+    for (size_t i = 2; i < 100000; ++i) {
+        some_map.insert(help.first, {10000000000 + i, "1000000000" + std::to_string(i)});
+    }
+
+    timeFinish = std::chrono::high_resolution_clock::now();
+    elapsed = timeFinish - timeStart;
+    std::cout << "Second example elapsed hint time: " << elapsed.count() << " s\n";   
 }
 
 int main() {
@@ -35,4 +63,6 @@ int main() {
         some_map[i] = std::to_string(i);
     }
     FirstExample(some_map);
+    std::cout << "\n\n";
+    SecondExample(some_map);
 }
